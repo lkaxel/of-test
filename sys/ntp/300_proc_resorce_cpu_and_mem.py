1c73f764af85d8be3e23e431aa400e21e0f3b9e2
root <likun@parksoon.com.cn>
2016-04-17 16:21:03 +0800

    change the fails' head

---
+++ sys/ntp/300_proc_resorce_cpu_and_mem.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+#!-*- coding:utf-8 -*-
+
+
+#import requests
+import time
+import json
+import socket
+import os
+
+data = []
+ts = int(time.time())
+host = os.uname()[1]
+
+def get_mem(value):
+	mem_cmd ="ps aux | grep " + str(value) + "|awk '{sum+=$4} END {print sum}'"
+	s = {}
+        s['endpoint'] = host
+        s['metric'] = 'mem_' + str(value)
+        s['timestamp'] = ts
+        s['step']= 60
+        s['value'] = float(os.popen(mem_cmd).read().strip())
+        s['counterType'] = 'GAUGE'
+        s['tags'] =  ''
+	data.append(s)
+
+
+def get_cpu(value):
+	cpu_cmd ="ps aux | grep " + str(value) + "|awk '{sum+=$3} END {print sum}'"
+	s = {}
+        s['endpoint'] = host
+        s['metric'] = 'cpu_' + str(value)
+        s['timestamp'] = ts
+        s['step']= 60
+        s['value'] = float(os.popen(cpu_cmd).read().strip())
+        s['counterType'] = 'GAUGE'
+        s['tags'] =  ''
+	data.append(s)
+processes = ['apache2', 'mysql', 'tomcat', '/usr/bin/java']
+
+for process in processes:
+	get_mem(process)
+#for process in processes:
+	get_cpu(process)
+print json.dumps(data)
